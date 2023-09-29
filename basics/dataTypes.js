d0c3e22e0f36922e087e1ded094594bb7d1d3b30
avinash singh <114334258+avinashkumar002@users.noreply.github.com>
2023-09-29 08:48:20 +0000

    dataypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,9 @@
+//string => ""
+// boolean => true/false
+// null => blank or zero valued
+// undefined => the value is not defined yet
+// symbol =>unique 
+// object
+
+// Type of undefined is undefined but the type of null is an object
+
