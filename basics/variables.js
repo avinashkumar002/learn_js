261787bd485bc6751316959042361d161fd9b5ec
avinash singh <114334258+avinashkumar002@users.noreply.github.com>
2023-09-29 08:40:42 +0000

    continer

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 14558
+let accontEmail = "hitesh@google.com"
+var accountPassword = "1289"
+accontCity = "jaipur"
+
+// accountId = 2 // not allowe
+
+accontEmail = "avinash@gmail.com"
+accountPassword = "420482"
+accontCity = "sasram"
+let accountCountry ;
+
+console.log([accountId , accontEmail , accountPassword , accontCity]);
+console.table([accountId , accontEmail , accountPassword , accontCity, accountCountry]);
