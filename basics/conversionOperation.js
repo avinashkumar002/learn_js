72770b0e49b85459330459c8238dc9731e2d4b9c
avinash singh <114334258+avinashkumar002@users.noreply.github.com>
2023-10-03 05:59:17 +0000

    conversion of datatypes

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,7 +1,16 @@
 let score = 33
 console.log(typeof score);
 
-let someNumber = 33 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33 
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**4);
+console.log(2/2);
+console.log(2%2);
+*/
+
