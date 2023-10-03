ef2d2e43193743e6ec79a270289a8610a1349b05
avinash singh <114334258+avinashkumar002@users.noreply.github.com>
2023-10-03 08:18:51 +0000

    data type summary

---
+++ basics/dataTypesSummary.js
@@ -0,0 +1,33 @@
+//Primitive
+// String , Number , Boolean , null , undefined , symbol(used for making unique variable or value) , BigInt
+
+
+const  score = 100;
+const scorevalue = 100.98
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+const bigNumber = 999999999n
+
+
+// Non-Primitive or Reference
+// Array , Objects , Functions
+
+const heros = ["shaktiman", "nagraj" , "doga"];
+
+let myObj = {
+    name: "avinash",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction);
