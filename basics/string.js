524a4908104b61310a0c0143067b544740ed1fa5
avinash singh <114334258+avinashkumar002@users.noreply.github.com>
2023-10-05 04:48:59 +0000

    String methods

---
+++ basics/string.js
@@ -0,0 +1,22 @@
+const name = "avinash";
+const repoCount = 50
+
+// console.log(name +repoCount +" Value");
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`); 
+
+const gameName = new String('Avinash-avi')
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4);
+console.log(anotherString);
+
+const newStringOne = "     Avinash    ";
+console.log(newStringOne.trim());
+
