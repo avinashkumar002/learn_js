f05673fdc6e7fac9267ead909de1451b5573ff44
avinash singh <114334258+avinashkumar002@users.noreply.github.com>
2023-10-08 03:53:34 +0000

    maths functions

---
+++ basics/numsAndMaths.js
@@ -0,0 +1,13 @@
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const Othernum = 23.8966;
+//console.log(Othernum.toPrecision(3));
+
+const min = 10;
+const max = 20 ;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
