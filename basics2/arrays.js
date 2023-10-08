0551cfe59cc440143a01c0d64c461b500cf595c7
avinash singh <114334258+avinashkumar002@users.noreply.github.com>
2023-10-08 07:36:34 +0000

    array operations

---
+++ basics2/arrays.js
@@ -0,0 +1,15 @@
+const myArr = [0 , 1 ,2, 3, 4 ,5]
+const myNames= ["avinash" , "Rahul"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+myArr.push(6);
+myArr.unshift(7);
+
+
+const newArr = myArr.join()
+console.log(newArr);
+console.log(myArr);
+
+
