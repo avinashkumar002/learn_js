a26fad493ab22f3811472f08f6c53893ff0b8a97
avinash singh <114334258+avinashkumar002@users.noreply.github.com>
2023-10-13 15:11:08 +0000

    scope in functions

---
+++ basics3/scopes.js
@@ -0,0 +1,20 @@
+let a = 300
+
+if(true){
+    let a = 10
+    const b = 20 
+    // console.log(a);
+}
+// console.log(a);
+function one(){
+    const username = "avi"
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+
